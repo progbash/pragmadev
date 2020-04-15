aa4fc37cc982a9f639de63ce6775bbd266851817
Kamran Mmmdv <amorfrap@gmail.com>
2020-04-15 12:58:17 +0400

    User's last_login is visible

--- pdapp/admin.py
+++ pdapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 from .models import Task, TaskFeedback
 
 # Register your models here.
@@ -9,7 +9,12 @@ class TaskAdmin(admin.ModelAdmin):
 class TaskFeedbackAdmin(admin.ModelAdmin):
     list_display = ['task', 'sender', 'is_solved', 'feedback_content', 'date_added']
 
+class CustomUserAdmin(admin.ModelAdmin):
+     list_display = ['first_name', 'last_name', 'last_login']
+
 admin.site.site_header = "Pragmadev - Admin Dashboard" 
 admin.site.register(Task, TaskAdmin)
 admin.site.register(TaskFeedback, TaskFeedbackAdmin)
 admin.site.unregister(Group)
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
